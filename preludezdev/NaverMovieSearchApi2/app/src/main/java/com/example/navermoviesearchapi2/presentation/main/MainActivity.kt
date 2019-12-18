31e661c4c874b69e37a56ccad0c35ad419ec521a
Taehwi Jeong <preludez.dev@gmail.com>
2019-12-19 00:45:01 +0900

    검색어 입력 부분 - EditText 디바운스 추가

--- preludezdev/NaverMovieSearchApi2/app/src/main/java/com/example/navermoviesearchapi2/presentation/main/MainActivity.kt
+++ preludezdev/NaverMovieSearchApi2/app/src/main/java/com/example/navermoviesearchapi2/presentation/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.navermoviesearchapi2.presentation.main
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.KeyEvent
 import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.Observer
@@ -12,34 +14,25 @@ import com.example.navermoviesearchapi2.R
 import com.example.navermoviesearchapi2.base.BaseActivity
 import com.example.navermoviesearchapi2.databinding.ActivityMainBinding
 import com.example.navermoviesearchapi2.presentation.detail.DetailActivity
+import com.example.navermoviesearchapi2.utils.plusAssign
 import io.reactivex.BackpressureStrategy
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.Subject
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.concurrent.TimeUnit
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
+    private val compositeDisposable = CompositeDisposable()
     private val backButtonSubject: Subject<Long> = BehaviorSubject.createDefault(0L).toSerialized()
-    private val backButtonSubjectDisposable: Disposable =
-        backButtonSubject.toFlowable(BackpressureStrategy.BUFFER)
-            .observeOn(AndroidSchedulers.mainThread())
-            .buffer(2, 1)
-            .map { it[0] to it[1] }
-            .subscribe({
-                when (it.second - it.first < BACK_BUTTON_TOAST_DURATION) {
-                    true -> finish()
-                    false -> showBackButtonToast()
-                }
-            }, {
-                showToastMessage("backButtonSubjectDisposable 에러 발생")
-            })
-
+    private val viewModel by viewModel<MainViewModel>()
     private val movieAdapter by lazy {
         MovieAdapter { clickEvent(it) }
     }
-    private val viewModel by viewModel<MainViewModel>()
 
     private fun clickEvent(url: String) {
         startActivity(Intent(this, DetailActivity::class.java).apply {
@@ -83,12 +76,55 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             hideKeyBoard()
             viewModel.searchDataByQuery()
         }
+
+        compositeDisposable += backButtonSubject.toFlowable(BackpressureStrategy.BUFFER)
+            .observeOn(AndroidSchedulers.mainThread())
+            .buffer(2, 1)
+            .map { it[0] to it[1] }
+            .subscribe({
+                when (it.second - it.first < BACK_BUTTON_TOAST_DURATION) {
+                    true -> finish()
+                    false -> showBackButtonToast()
+                }
+            }, {
+                showToastMessage("backButtonSubjectDisposable 에러 발생")
+            })
     }
 
     private fun initCallback() {
         viewModel.notificationMsg.observe(this, Observer {
             it.getContentIfNotHandled()?.let { msg -> showToastMessage(msg) }
         })
+
+        val source = Observable.create<CharSequence> { emitter ->
+            etSearch.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                }
+
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    s?.let {
+                        emitter.onNext(it)
+                    }
+                }
+            })
+        }
+
+        compositeDisposable += source.debounce(QUERY_INPUT_TIME_OUT, TimeUnit.MILLISECONDS)
+            .filter { it.isNotEmpty() }
+            .observeOn(AndroidSchedulers.mainThread()) //Toast must be running on UI Thread
+            .subscribe {
+                showToastMessage("$it 검색 중...")
+                hideKeyBoard()
+                viewModel.searchDataByQuery()
+            }
     }
 
     private fun hideKeyBoard() {
@@ -106,12 +142,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     override fun onDestroy() {
-        backButtonSubjectDisposable.dispose()
+        compositeDisposable.dispose()
         super.onDestroy()
     }
 
     companion object {
         const val KEY_URL = "KEY_URL"
         const val BACK_BUTTON_TOAST_DURATION = 1500L
+        const val QUERY_INPUT_TIME_OUT = 3000L
     }
 }
