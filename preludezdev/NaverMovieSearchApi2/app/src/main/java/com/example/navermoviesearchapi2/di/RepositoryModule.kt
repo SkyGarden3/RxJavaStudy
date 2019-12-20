ccb9b4ad06b54b6410c18e504f61edede7a2b366
Taehwi Jeong <preludez.dev@gmail.com>
2019-12-21 01:05:38 +0900

    networkModule name 명시 삭제

--- preludezdev/NaverMovieSearchApi2/app/src/main/java/com/example/navermoviesearchapi2/di/RepositoryModule.kt
+++ preludezdev/NaverMovieSearchApi2/app/src/main/java/com/example/navermoviesearchapi2/di/RepositoryModule.kt
@@ -4,10 +4,9 @@ import com.example.navermoviesearchapi2.data.MovieRepository
 import com.example.navermoviesearchapi2.data.source.MovieRepositoryImpl
 import com.example.navermoviesearchapi2.data.source.remote.MovieRemoteDataSource
 import com.example.navermoviesearchapi2.data.source.remote.MovieRemoteDataSourceImpl
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single { MovieRepositoryImpl(get(named("Remote"))) as MovieRepository }
-    single(named("Remote")) { MovieRemoteDataSourceImpl(get()) as MovieRemoteDataSource }
+    single<MovieRepository> { MovieRepositoryImpl(get()) }
+    single<MovieRemoteDataSource> { MovieRemoteDataSourceImpl(get()) }
 }
