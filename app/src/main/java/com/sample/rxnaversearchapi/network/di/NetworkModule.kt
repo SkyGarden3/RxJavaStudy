a1fa0a8dca8683cfdf5012c62b822c15a1e7d753
wswon <dnjsdntjr1@gmail.com>
2019-12-26 16:48:38 +0900

    interceptor로 retrofit header 분리

--- app/src/main/java/com/sample/rxnaversearchapi/network/di/NetworkModule.kt
+++ app/src/main/java/com/sample/rxnaversearchapi/network/di/NetworkModule.kt
@@ -1,7 +1,13 @@
 package com.sample.rxnaversearchapi.network.di
 
 import com.sample.rxnaversearchapi.network.api.MovieSearchApi
+import com.sample.rxnaversearchapi.network.api.NaverApiKey
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
+import retrofit2.CallAdapter
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -12,13 +18,32 @@ val networkModule = module {
             .baseUrl("https://openapi.naver.com/")
             .addConverterFactory(get())
             .addCallAdapterFactory(get())
+            .client(get())
             .build()
             .create(MovieSearchApi::class.java)
     }
-    single {
+
+    single<Converter.Factory> {
         GsonConverterFactory.create()
     }
-    single {
+
+    single<CallAdapter.Factory> {
         RxJava2CallAdapterFactory.create()
+    }
+
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor {
+                it.proceed(get { parametersOf(it) })
+            }
+            .build()
+    }
+
+    single { (chain: Interceptor.Chain) ->
+        chain.request().newBuilder()
+            .header("X-Naver-Client-Id", NaverApiKey.CLIENT_ID)
+            .header("X-Naver-Client-Secret", NaverApiKey.SECRET_KEY)
+            .method(chain.request().method(), chain.request().body())
+            .build()
     }
 }
